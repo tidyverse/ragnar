b6c29061bf69e5b7827b87ef590a84cd4f95722f
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-02-11 05:50:58 -0500

    add `py_to_r` method for `markitdown.DocumentConverterResult`

--- R/ragnar-package.R
+++ R/ragnar-package.R
@@ -4,6 +4,12 @@
 
 .globals <- new.env(parent = emptyenv())
 
+#' @exportS3Method reticulate::py_to_r
+py_to_r.markitdown._markitdown.DocumentConverterResult <- function(x) {
+  c(title = x$title, text = x$text_content)
+}
+
+
 .onLoad <- function(libname, pkgname) {
   reticulate::py_require("markitdown")
   Sys.setenv(RETICULATE_PYTHON = "managed")
