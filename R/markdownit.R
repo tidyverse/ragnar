ec7615d7c70ffac4140ac00a661200fc9160381f
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-02-17 10:48:53 -0500

    implement and add `markdown_split()`

--- R/markdownit.R
+++ R/markdownit.R
@@ -30,7 +30,69 @@ split_markdown <- function(md, split_by = c("h1", "h2", "h3", "pre", "p")) {
              split_tags = split_by)
 }
 
-frame_markdown <- function(md, frame_by = c("h1", "h2", "h3"), split_by = c("p", "pre")) {
+markdown_split <- function(text, tags = c("h1", "h2", "h3"), trim = TRUE, omit_empty = TRUE) {
+
+  lines <- text |> stri_split_lines() |> unlist() |> stri_c("\n")
+  text <- lines |> stri_flatten()
+
+  doc <- text |>
+    commonmark::markdown_html(sourcepos = TRUE, extensions = TRUE, normalize = TRUE) |>
+    read_html(encoding = "UTF-8")
+
+  elements <- doc |> xml_find_all(xpath = "//*[@data-sourcepos]")
+
+  df <- tibble::tibble(
+    tag = elements |> xml_name(),
+    source_position = elements |> xml_attr("data-sourcepos"),
+    text = elements |> xml_text()
+  )
+
+  df <- dplyr::filter(df, tag %in% unique(c(tags)))
+
+  # common mark returns positions as line:byte-line:byte
+  # e.g., 52:1-52:20
+  position <- df$source_position |>
+    stringi::stri_split_charclass("[-:]", n = 4L, simplify = TRUE)
+  storage.mode(position) <- "integer"
+  colnames(position) <- c("start_line", "start_byte", "end_line", "end_byte")
+
+  line_numbytes <- stri_numbytes(lines)
+  line_startbyte <- c(1L, 1L + drop_last(cumsum(line_numbytes)))
+
+  bytes <- charToRaw(text)
+
+  start <- line_startbyte[position[, "start_line"]] + position[, "start_byte"] - 1L # position of start byte, inclusive
+  end <- line_startbyte[position[, "end_line"]] + position[, "end_byte"] - 1L # position of end byte, inclusive
+  cuts <- sort(unique(c(rbind(start, end + 1L))))
+
+  nms <- c("", df$tag[match(cuts, start)])
+  nms[is.na(nms)] <- ""
+
+  # if(FALSE) {
+  #   np <- reticulate::import("numpy", convert = F)
+  #   splits <- np$split(
+  #     as.array(charToRaw(text)),
+  #     as.array(cuts - 1L)
+  #   ) |> py_to_r() |> vapply(rawToChar, "")
+  # }
+
+  sizes <- c(cuts, length(bytes) + 1L) - c(1L, cuts)
+  splits <- vctrs::vec_chop(bytes, sizes = sizes) |> vapply(rawToChar, "")
+
+  if(trim)
+    splits <- stri_trim_both(splits)
+
+  names(splits) <- nms
+
+  if(omit_empty) {
+    splits <- splits[nzchar(splits) | nzchar(names(splits))]
+  } else if (cuts[1L] != 1L) {
+    splits <- splits[-1L]
+  }
+  splits
+
+}
+
   md <- split_markdown(md, split_by = unique(c(frame_by, split_by)))
   frame <- vec_frame_flattened_tree(md, frame_by, names = "tag", leaves = "text")
   if (base::setequal(split_by, frame_by))
