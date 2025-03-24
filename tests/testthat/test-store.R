c0b7941443dcbb334301d0209868672571d724ce
Daniel Falbel <dfalbel@gmail.com>
2025-03-24 15:08:44 -0300

    Add a name for stores to use (#12)
    * Add a name for stores to use
    
    * Scorrect store test

--- tests/testthat/test-store.R
+++ tests/testthat/test-store.R
@@ -1,5 +1,6 @@
 test_that("ragnar_store_update/insert", {
   store <- ragnar_store_create(embed = \(x) matrix(nrow = length(x), ncol = 100, stats::runif(100)))
+  expect_true(grepl("^store_[0-9]+$", store@name))
 
   chunks <- data.frame(
     origin = "foo",
