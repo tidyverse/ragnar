2d5208eced3d05092867887e0f209272d82acbe8
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-06-03 08:21:54 -0400

    add tests

--- tests/testthat/test-read-markdown.R
+++ tests/testthat/test-read-markdown.R
@@ -40,3 +40,25 @@ test_that("ragnar_read() empty doc", {
   jpg <- file.path(R.home("doc"), "html", "logo.jpg")
   expect_no_error(ragnar_read(jpg, frame_by_tags = "h1"))
 })
+
+
+test_that("markitdown patches", {
+  skip_if_offline()
+  url = "https://quarto.org/docs/computations/r.html"
+  htmlfile <- withr::local_tempfile(fileext = ".html")
+  download.file(url, htmlfile, quiet = TRUE)
+
+  # ensure that 'main_only' returns a shorter document (omitting sidebar)
+  main_only <- read_as_markdown(htmlfile, main_only = TRUE)
+  not_main_only <- read_as_markdown(htmlfile, main_only = FALSE)
+  expect_lt(stri_length(main_only), stri_length(not_main_only))
+
+  # ensure that 'main_only' is a strict slice of the full converted document
+  # (modulo the page title)
+  main_only_sans_title <- stri_replace_first_regex(main_only, "^# .*\n+", "")
+  expect_true(stri_detect_fixed(not_main_only, main_only_sans_title))
+
+  # ensure that some fences have been expanded
+  expect_true(stri_detect_fixed(main_only, "````"))
+  expect_true(stri_detect_fixed(not_main_only, "````"))
+})
