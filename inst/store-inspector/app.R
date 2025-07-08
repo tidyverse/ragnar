f2cd75478ef022904b75689e998362db9bf1ae7d
Daniel Falbel <dfalbel@gmail.com>
2025-07-08 09:02:33 -0300

    run app inside local({})

--- inst/store-inspector/app.R
+++ inst/store-inspector/app.R
@@ -1,19 +1,21 @@
 # see ragnar_store_inspect() for instructions tousing the app
-source("modules.R")
-store <- getOption("ragnar_inspector_store", NULL)
+local({
+  source("modules.R")
+  store <- getOption("ragnar_inspector_store", NULL)
 
-if (is.null(store@embed)) {
-  search_types <- "BM25"
-} else {
-  search_types <- c("VSS", "BM25")
-}
-
-ui <- storeInspectorUI("ragnarInspector", search_types)
-server <- function(input, output, session) {
-  if (is.null(store)) {
-    stop("No store provided")
+  if (is.null(store@embed)) {
+    search_types <- "BM25"
+  } else {
+    search_types <- c("VSS", "BM25")
   }
-  storeInspectorServer("ragnarInspector", store)
-}
 
-shiny::shinyApp(ui, server)
+  ui <- storeInspectorUI("ragnarInspector", search_types)
+  server <- function(input, output, session) {
+    if (is.null(store)) {
+      stop("No store provided")
+    }
+    storeInspectorServer("ragnarInspector", store)
+  }
+
+  shiny::shinyApp(ui, server)
+})
