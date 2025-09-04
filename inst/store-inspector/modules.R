57c1100773b51a4bcf4242fd53bac161a9a82115
Daniel Falbel <dfalbel@gmail.com>
2025-09-04 16:15:18 -0300

    fix xml (#117)

--- inst/store-inspector/modules.R
+++ inst/store-inspector/modules.R
@@ -114,7 +114,7 @@ storeInspectorServer <- function(id, store) {
           lapply(function(node) {
             tryCatch(
               {
-                if (xml_name(node) == "a") return()
+                if (xml2::xml_name(node) == "a") return()
 
                 text <- as.character(node)
                 text <- stringi::stri_replace_all(
