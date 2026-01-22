72e3bf065e03f17946a7311f3bd6d4ddcd464a06
Shuai Wu <69974660+Rednose22@users.noreply.github.com>
2026-01-22 23:17:50 +0000

    Correct the typo in markdown-chunk.R (#160)
    * Update markdown-chunk.R
    
    Correct the typo
    
    * Update markdown-chunk.R
    
    correct the typo
    
    * `air format .`
    
    ---------
    
    Co-authored-by: Tomasz Kalinowski <kalinowskit@gmail.com>

--- R/markdown-chunk.R
+++ R/markdown-chunk.R
@@ -413,7 +413,7 @@ markdown_position_headings <- function(
 
   df |>
     mutate(
-      nearest_preceeding_heading_level = md_headings$level[
+      nearest_preceding_heading_level = md_headings$level[
         findInterval(
           position,
           md_headings$start,
@@ -426,7 +426,7 @@ markdown_position_headings <- function(
       ],
       max_applicable_level = coalesce(
         start_of_heading_level - 1L,
-        nearest_preceeding_heading_level,
+        nearest_preceding_heading_level,
         99L
       ),
       heading = map2(heading, max_applicable_level, \(h, l) {
@@ -449,7 +449,7 @@ utils::globalVariables(c(
   "heading_start",
   "closest",
   "start_of_heading_level",
-  "nearest_preceeding_heading_level",
+  "nearest_preceding_heading_level ",
   "heading",
   "max_applicable_level"
 ))
